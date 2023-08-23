babab7263806ae9d7fb18e6f35451538b31856ef
balesetiawan0910 <setiawann@alumni.pcr.ac.id>
2023-08-23 17:54:51 +0700

    add new setiawan

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Earn Bale
+          Earn Bale Setiawan
         </a>
       </header>
     </div>
